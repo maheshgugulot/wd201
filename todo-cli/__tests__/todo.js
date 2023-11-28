fb2e8b0b91b99edbbc0f362267db1f74037a8423
Mahesh <maheshgugulot@gmail.com>
2023-11-28 16:39:43 +0530

    update __test__/todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -4,55 +4,45 @@ const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
 
 const createTodo = (title, dueDate) => ({ title, dueDate, completed: false });
 const dateToday = new Date();
-const today = dateToday.toISOString().split("T")[0];
+const today = dateToday.toLocaleDateString("en-CA");
 const yesterday = new Date(dateToday);
 yesterday.setDate(dateToday.getDate() - 1);
-const Yesterday = yesterday.toISOString().split("T")[0];
+const Yesterday = yesterday.toLocaleDateString("en-CA");
 
 const tomorrow = new Date(dateToday);
 tomorrow.setDate(dateToday.getDate() + 1);
-const Tomorrow = tomorrow.toISOString().split("T")[0];
+const Tomorrow = tomorrow.toLocaleDateString("en-CA");
 beforeAll(() => {
   add(createTodo("Test Todo 1", today));
-  add(createTodo("Test Todo 2", Tomorrow));
-  add(createTodo("Test Todo 3", Yesterday));
 });
 test("adds a new todo", () => {
   const itemcount = all.length;
-  const newTodoItem = createTodo("New Test Todo", Yesterday);
+  const newTodoItem = createTodo("New Test Todo", today);
   add(newTodoItem);
   const newlen = all.length;
   expect(newlen).toBe(itemcount + 1);
 });
 
 test("marks a todo as complete", () => {
+  expect(all[0].completed).toBe(false);
   markAsComplete(0);
   expect(all[0].completed).toBe(true);
 });
 
 test("retrieves overdue items", () => {
   const overdueItems = overdue();
-  expect(
-    overdueItems.every((t) => {
-      return t.dueDate < today;
-    }),
-  ).toBe(true);
+  add(createTodo("Test Todo 2", Yesterday));
+  expect(overdue().length).toBe(overdueItems.length + 1);
 });
 
 test("retrieves due today items", () => {
   const dueTodayItems = dueToday();
-  expect(
-    dueTodayItems.every((t) => {
-      return t.dueDate === today;
-    }),
-  ).toBe(true);
+  add(createTodo("Test Todo 3", today));
+  expect(dueToday().length).toBe(dueTodayItems.length + 1);
 });
 
 test("retrieves due later items", () => {
   const dueLaterItems = dueLater();
-  expect(
-    dueLaterItems.every((t) => {
-      return t.dueDate > today;
-    }),
-  ).toBe(true);
+  add(createTodo("Test Todo 4", Tomorrow));
+  expect(dueLater().length).toBe(dueLaterItems.length + 1);
 });
